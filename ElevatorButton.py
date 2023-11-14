9e84197387554267bb5e0492fc3d84bd6a5d7b6b
luisnisc <luisnisc30mayo2005@gmail.com>
2023-11-14 11:46:58 +0100

    .

--- ElevatorButton.py
+++ ElevatorButton.py
@@ -16,42 +16,62 @@ class ElevatorButton:
 
     def toggle(self):
         if self.estado == "on":
-            self.estado = "off"
+            self.estado = "off" 
         else:
             self.estado = "on"
 
     def __str__(self):
-        return f"{self.label}: {self.estado}"
-
-
+        respuesta= "BBX= ["
+        #contenido de la cadena (TODO)
+        respuesta = "]"
+        # return f"{self.label}: {self.estado}"
+    
+class elevatorButtonBox:
+    def __init__(self, tam):
+        self.tamMax = tam
+        self.tamActual= 0
+        self.lista = []  
+    def __str__(self):
+        return ', '.join(str(boton) for boton in self.lista)
+    def add(self, boton):
+        if self.tamActual < self.tamMax:
+            self.lista.append(boton)
+            self.tamMax-1
+            
 if __name__ == "__main__":
 
     eb = ElevatorButton("UP")
 
-    print(eb)  # off
+    # print(eb)  # off
 
-    eb.toggle()
-    print(eb)  # on
+    # eb.toggle()
+    # print(eb)  # on
 
-    eb.setOn()
-    print(eb)  # on
+    # eb.setOn()
+    # print(eb)  # on
 
-    eb.toggle()
-    print(eb)  # off
-    eb.setOff()
-    print(eb)  # off
+    # eb.toggle()
+    # print(eb)  # off
+    # eb.setOff()
+    # print(eb)  # off
 
     # botoneras ... class ElevatorButtonBox ...
 
     ebU = ElevatorButton("UP")
     ebD = ElevatorButton("DOWN")
 
-    wallButonBox = []
-    wallButonBox.append(ebU)
-    wallButonBox.append(ebD)
+    # wallButonBox = []
+    # wallButonBox.append(ebU)
+    # wallButonBox.append(ebD)
 
-    cabinButtonBox = []
-    cabinButtonBox.append(ElevatorButton("1"))
-    cabinButtonBox.append(ElevatorButton("2"))
-    cabinButtonBox.append(ElevatorButton("3"))
+    # cabinButtonBox = []
+    # cabinButtonBox.append(ElevatorButton("1"))
+    # cabinButtonBox.append(ElevatorButton("2"))
+    # cabinButtonBox.append(ElevatorButton("3"))
+    
+    bbx = elevatorButtonBox(3)
+    bbx.add(ebD)
+    print(bbx)
+    bbx.add(ebU)
+    print(bbx)
     
